ebfdfb6b1ee08dc0fc80d300cb33bb370cd004e0
Elvin Mammadov <elvinmammadoff@gmail.com>
2024-02-20 21:15:59 +0400

    doc

---
+++ xtremeup-noImages/xtremeup-development-version/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+      animation: {
+        bounce: "bounce 1s infinite",
+      },
+      keyframes: {
+        bounce: {
+          "0%,100%": {
+              transform: "translateY(-25%)",
+              animationTimingFunction: "cubic-bezier(.8,0,1,1)"
+          },
+          "50%": {
+              transform: "translateY(0)",
+              animationTimingFunction: "cubic-bezier(0,0,.2,1)"
+          }
+        }
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
